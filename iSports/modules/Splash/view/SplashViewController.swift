6dea8a429769e23e1cf75885214aa309216f3875
ELDemy <mahmoudeldemerdash5@gmail.com>
2026-06-01 16:27:43 +0300

    add splash screen with lottie animation

---
+++ iSports/modules/Splash/view/SplashViewController.swift
@@ -0,0 +1,59 @@
+//
+//  SplashViewController.swift
+//  iSports
+//
+//  Created by JETSMobileLabMini9 on 01/06/2026.
+//
+
+import Lottie
+import UIKit
+
+class SplashViewController: UIViewController {
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    private var animationView: LottieAnimationView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBackground
+        setupAnimation()
+    }
+
+    private func setupAnimation() {
+        animationView = LottieAnimationView(name: "SplashAnimation")
+
+        guard let animationView = animationView else { return }
+
+        animationView.frame = view.bounds
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .playOnce
+
+        view.addSubview(animationView)
+
+        animationView.play { [weak self] (finished) in
+            if finished {
+                self?.transitionToMainScreen()
+            }
+        }
+    }
+
+    private func transitionToMainScreen() {
+        let mainVC = UIStoryboard(name: "Main", bundle: nil)
+            .instantiateViewController(withIdentifier: "MainViewController")
+        mainVC.modalPresentationStyle = .fullScreen
+
+        self.present(mainVC, animated: true, completion: nil)
+
+    }
+
+}
